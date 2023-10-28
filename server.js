49eaf705c2e2f06e0810d3a36de191b1c51edff5
toukirraju <toukirraju@gmail.com>
2023-10-28 17:35:58 +0600

    course-list

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+const router = jsonServer.router("db.json");
+
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
+// Bind the router db to the app
+app.db = router.db;
+
+app.use(middlewares);
+
+app.use(router);
+
+server.listen(port);
